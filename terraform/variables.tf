f63ee91c4f5177e551fd73f2b90ff7d91660ea61
Eren Sanlıer <erensanlier@gmail.com>
2024-05-10 17:53:55 +0300

    remove unnecessary variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,16 +1,3 @@
-variable "backend_hostname" {
-  type    = string
-  default = "app.terraform.io"
-}
-
-variable "organization" {
-  type = string
-}
-
-variable "workspace_prefix" {
-  type = string
-}
-
 variable "region" {
   type    = string
   default = "us-east-1"
