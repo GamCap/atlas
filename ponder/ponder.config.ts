02a950228fc1472968cc748e74ec51b73ca1d8ef
Eren Sanlıer <erensanlier@gmail.com>
2024-06-12 23:08:34 +0300

    tree update

--- ponder/ponder.config.ts
+++ ponder/ponder.config.ts
@@ -2,15 +2,16 @@ import { createConfig, loadBalance } from "@ponder/core";
 import { http, webSocket } from "viem";
 import { OptimismMintableERC20 } from "./abis/OptimismMintableERC20";
 import { WLDERC20 } from "./abis/WLDERC20";
+import { WorldIDIdentityManager2 } from "./abis/WorldIDIdentityManager2";
 
 export default createConfig({
-  database: {
-    kind: "postgres",
-    poolConfig: {
-      max: 300
-    },
-    schema: "ponder_data",
-  },
+  //database: {
+  //  kind: "postgres",
+  //  poolConfig: {
+  //    max: 300
+  //  },
+  //  schema: "ponder_data",
+  //},
   networks: {
     mainnet: {
       chainId: 1,
@@ -24,22 +25,38 @@ export default createConfig({
     },
   },
   contracts: {
-    WLD_Optimism: {
-      network: "optimism",
-      abi: OptimismMintableERC20,
-      address: "0xdC6fF44d5d932Cbd77B52E5612Ba0529DC6226F1",
-      startBlock: 107087966,
-      filter: {
-        event: "Transfer"
-      }
-    },
-    WLD_Ethereum: {
+    //WLD_Optimism: {
+    //  network: "optimism",
+    //  abi: OptimismMintableERC20,
+    //  address: "0xdC6fF44d5d932Cbd77B52E5612Ba0529DC6226F1",
+    //  startBlock: 107087966,
+    //  endBlock: 107088066,
+    //  filter: {
+    //    event: "Transfer"
+    //  }
+    //},
+    //WLD_Ethereum: {
+    //  network: "mainnet",
+    //  abi: WLDERC20,
+    //  address: "0x163f8C2467924be0ae7B5347228CABF260318753",
+    //  startBlock: 17714705,
+    //  endBlock: 17784805,
+    //  filter: {
+    //    event: "Transfer"
+    //  }
+    //},
+    WorldIDIdentityManager2: {
       network: "mainnet",
-      abi: WLDERC20,
-      address: "0x163f8C2467924be0ae7B5347228CABF260318753",
-      startBlock: 17714705,
+      abi: WorldIDIdentityManager2,
+      address: "0xf7134CE138832c1456F2a91D64621eE90c2bddEa",
+      startBlock: 17634324,
+      endBlock: 17646832,
       filter: {
-        event: "Transfer"
+        event: "TreeChanged",
+        args: {
+          from: "0x9ad4EFAF9E326c17c3A7be6F5D167843Af0eb30A",
+          to: ["0xf7134CE138832c1456F2a91D64621eE90c2bddEa", "0x316350D3EC608fFc30b01DcB7475De1C676cE910"]
+        },
       }
     },
   },
--- ponder/ponder.schema.ts
+++ ponder/ponder.schema.ts
@@ -1,36 +1,45 @@
 import { createSchema } from "@ponder/core";
 
 export default createSchema((p) => ({
-  TransferEventOptimism: p.createTable({
-    id: p.string(),
-    amount: p.bigint(),
-    fromId: p.string().references("AccountOptimism.id"),
-    toId: p.string().references("AccountOptimism.id"),
+  RootKind: p.createEnum(["Insert", "Remove"]),
+
+  Root: p.createTable({
+    id: p.bigint(),
+    kind: p.enum("RootKind").optional(),
+    preRoot: p.bigint().references("Root.id").optional(),
+    postRoot: p.bigint().references("Root.id").optional(),
     timestamp: p.int(),
-    from: p.one("fromId"),
-    to: p.one("toId"),
   }),
-  AccountOptimism: p.createTable({
-    id: p.string(),
-    balance: p.bigint(),
-    isOwner: p.boolean(),
-    transferFromEvents: p.many("TransferEventOptimism.fromId"),
-    transferToEvents: p.many("TransferEventOptimism.toId"),
-  }),
-  TransferEventEthereum: p.createTable({
-    id: p.string(),
-    amount: p.bigint(),
-    fromId: p.string().references("AccountEthereum.id"),
-    toId: p.string().references("AccountEthereum.id"),
-    timestamp: p.int(),
-    from: p.one("fromId"),
-    to: p.one("toId"),
-  }),
-  AccountEthereum: p.createTable({
-    id: p.string(),
-    balance: p.bigint(),
-    isOwner: p.boolean(),
-    transferFromEvents: p.many("TransferEventEthereum.fromId"),
-    transferToEvents: p.many("TransferEventEthereum.toId"),
-  })
+  //TransferEventOptimism: p.createTable({
+  //  id: p.string(),
+  //  amount: p.bigint(),
+  //  fromId: p.string().references("AccountOptimism.id"),
+  //  toId: p.string().references("AccountOptimism.id"),
+  //  timestamp: p.int(),
+  //  from: p.one("fromId"),
+  //  to: p.one("toId"),
+  //}),
+  //AccountOptimism: p.createTable({
+  //  id: p.string(),
+  //  balance: p.bigint(),
+  //  isOwner: p.boolean(),
+  //  transferFromEvents: p.many("TransferEventOptimism.fromId"),
+  //  transferToEvents: p.many("TransferEventOptimism.toId"),
+  //}),
+  //TransferEventEthereum: p.createTable({
+  //  id: p.string(),
+  //  amount: p.bigint(),
+  //  fromId: p.string().references("AccountEthereum.id"),
+  //  toId: p.string().references("AccountEthereum.id"),
+  //  timestamp: p.int(),
+  //  from: p.one("fromId"),
+  //  to: p.one("toId"),
+  //}),
+  //AccountEthereum: p.createTable({
+  //  id: p.string(),
+  //  balance: p.bigint(),
+  //  isOwner: p.boolean(),
+  //  transferFromEvents: p.many("TransferEventEthereum.fromId"),
+  //  transferToEvents: p.many("TransferEventEthereum.toId"),
+  //})
 }));
