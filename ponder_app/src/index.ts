b2e2692a8bb89030be7cd11cc6e9c7471d52712d
Eren Sanlıer <erensanlier@gmail.com>
2024-07-30 19:10:39 +0300

    disable WLD indexing

--- ponder_app/src/index.ts
+++ ponder_app/src/index.ts
@@ -24,71 +24,71 @@ ponder.on("WorldIDIdentityManager:TreeChanged", async ({ event, context }) => {
 	}
 });
 
-ponder.on("WLD:Transfer", async ({ event, context }) => {
-	const { Account, TransferEvent } = context.db;
-  
-	// Create an Account for the sender, or update the balance if it already exists.
-	await Account.upsert({
-	  id: event.args.from,
-	  create: {
-		balance: BigInt(0),
-		isOwner: false,
-	  },
-	  update: ({ current }) => ({
-		balance: current.balance - event.args.value,
-	  }),
-	});
-
-// Create an Account for the recipient, or update the balance if it already exists.
-	await Account.upsert({
-	  id: event.args.to,
-	  create: {
-		balance: event.args.value,
-		isOwner: false,
-	  },
-	  update: ({ current }) => ({
-		balance: current.balance + event.args.value,
-	  }),
-	});
-  
-// Create a TransferEvent.
-	await TransferEvent.create({
-	  id: event.log.id,
-	  data: {
-		fromId: event.args.from,
-		toId: event.args.to,
-		amount: event.args.value,
-		timestamp: Number(event.block.timestamp),
-	  },
-	});
-  });
-  
-  ponder.on("WLD:Approval", async ({ event, context }) => {
-	const { Allowance, ApprovalEvent } = context.db;
-  
-	const allowanceId = `${event.args.owner}-${event.args.spender}`;
-  
-// Create or update the Allowance.
-	await Allowance.upsert({
-	  id: allowanceId,
-	  create: {
-		ownerId: event.args.owner,
-		spenderId: event.args.spender,
-		amount: event.args.value,
-	  },
-	  update: {
-		amount: event.args.value,
-	  },
-	});
-  
-// Create an ApprovalEvent.
-	await ApprovalEvent.create({
-	  id: event.log.id,
-	  data: {
-		ownerId: event.args.owner,
-		spenderId: event.args.spender,
-		amount: event.args.value,
-		timestamp: Number(event.block.timestamp),
-	  },
-	});
-  });
+// ponder.on("WLD:Transfer", async ({ event, context }) => {
+// 	const { Account, TransferEvent } = context.db;
+//
+// 	// Create an Account for the sender, or update the balance if it already exists.
+// 	await Account.upsert({
+// 	  id: event.args.from,
+// 	  create: {
+// 		balance: BigInt(0),
+// 		isOwner: false,
+// 	  },
+// 	  update: ({ current }) => ({
+// 		balance: current.balance - event.args.value,
+// 	  }),
+// 	});
+//
+// // Create an Account for the recipient, or update the balance if it already exists.
+// 	await Account.upsert({
+// 	  id: event.args.to,
+// 	  create: {
+// 		balance: event.args.value,
+// 		isOwner: false,
+// 	  },
+// 	  update: ({ current }) => ({
+// 		balance: current.balance + event.args.value,
+// 	  }),
+// 	});
+//
+// // Create a TransferEvent.
+// 	await TransferEvent.create({
+// 	  id: event.log.id,
+// 	  data: {
+// 		fromId: event.args.from,
+// 		toId: event.args.to,
+// 		amount: event.args.value,
+// 		timestamp: Number(event.block.timestamp),
+// 	  },
+// 	});
+//   });
+//
+//   ponder.on("WLD:Approval", async ({ event, context }) => {
+// 	const { Allowance, ApprovalEvent } = context.db;
+//
+// 	const allowanceId = `${event.args.owner}-${event.args.spender}`;
+//
+// // Create or update the Allowance.
+// 	await Allowance.upsert({
+// 	  id: allowanceId,
+// 	  create: {
+// 		ownerId: event.args.owner,
+// 		spenderId: event.args.spender,
+// 		amount: event.args.value,
+// 	  },
+// 	  update: {
+// 		amount: event.args.value,
+// 	  },
+// 	});
+//
+// // Create an ApprovalEvent.
+// 	await ApprovalEvent.create({
+// 	  id: event.log.id,
+// 	  data: {
+// 		ownerId: event.args.owner,
+// 		spenderId: event.args.spender,
+// 		amount: event.args.value,
+// 		timestamp: Number(event.block.timestamp),
+// 	  },
+// 	});
+//   });
